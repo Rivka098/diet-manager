6cc8045ec29811d13729903e37ead0ca2d8be342
Rivka098 <rivka20180@gmail.com>
2026-06-01 17:22:18 +0300

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
-  render(<App />);
-  const title = screen.getByText(/מנהלת הדיאטה שלי/i);
-  expect(title).toBeInTheDocument();
-});
